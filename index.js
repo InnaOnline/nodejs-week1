b777d11a00e55b0b1c2bcb0ebc4ac15e36227336
unknown <https://github.com/InnaOnline/todo.git>
2021-12-08 15:30:38 +0300

    homework1

--- index.js
+++ index.js
@@ -1,87 +1,164 @@
 const yargs = require('yargs')
-const fs = require("fs");
-const util = require("util");
-const path = require("path");
-const readdir = util.promisify(fs.readdir);
-
-const directory = process.argv[2];
-const finalPath = process.argv[3];
-const shouldDelete = process.argv[4];
+const { readdir, stat, copyFile, mkdir, rmdir, unlink } = require('fs')
+const path = require('path')
+let counter = 0
 
 const args = yargs
-    .usage('Usage: node $0 [options]')
-    .help('help')
-    .alias('help', 'h')
-    .version('1.0.1')
-    .alias('version', 'v')
-    .example('node $0 ./path ./path -d')
-    .option('delete', {
-         alias: 'd',
-         describe: 'Удалить ли папку ?',
-         default: false,
-         boolean: true
-    })
-    .epilog('Homework 1')
-    .argv
+  .usage("Usage: node $0 [options]")
+  .help('help')
+  .alias('help', 'h')
+  .version('1.0.0')
+  .alias('version', 'v')
+  .example('node $0 --entry [path]', 'File sorted')
+  .option('entry', {
+    alias: 'e',
+    describe: 'Start folder path',
+    demandOption: true
+  })
+  .option('dist', {
+    alias: 'd',
+    describe: "Result folder path",
+    default: '/output'
+  })
+  .option('delete', {
+    alias: "D",
+    describe: 'Remove start folder?',
+    type: 'boolean',
+    default: false,
+    boolean: true
+  })
+  .epilog('Homework 1')
+  .argv
 
-    const sort = (directory, err) => {
-    const files = fs.readdir(directory, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
+const config = {
+    entry: path.resolve(__dirname, args.entry),
+    dist: path.resolve(__dirname, args.dist),
+    delete: args.delete
+}
 
-    files.forEach(file => {
-        const localBase = path.join(directory, file);
-        const state = fs.statSync(localBase);
+const folder = path.normalize(path.join(__dirname, args.entry))
+const dist = path.normalize(path.join(__dirname, args.output))
 
-        if (state.isDirectory()) {
-            sort(localBase, finalPath);
-        } else {
-            if (!fs.existsSync(`./${finalPath}`)) {
-                fs.mkdirSync(`./${finalPath}`);
-            }
+class Observer {
+    constructor(cb) {
+        this.observers = new Proxy([], {
+            set(target, prop, value) {
+                target[prop] = value
 
-            if (!fs.existsSync(path.join(finalPath, file[0].toUpperCase()))) {
-                fs.mkdirSync(path.join(finalPath, file[0].toUpperCase()));
-            }
-
-            fs.link(
-                localBase,
-                path.join(finalPath, file[0].toUpperCase(), file),
-                (err) => {
-                    if (err) {
-                        throw new Error(err);
-                    }
+                if (target.length === 0) {
+                    cb()
                 }
-            );
+
+                return true
+            }
+        })
+    }
+
+    add(observer) {
+        this.observers.push(observer)
+    }
+
+    remove(observer) {
+        const index = this.observers.findIndex(obs => obs === observer)
+
+        this.observers.splice(index, 1)
+    }
+}
+
+const observer = new Observer(() => {
+    deleteFolder(folder)
+})
+
+function deleteFolder(folder) {
+    (function recursive(src) {
+        if (src === path.resolve(folder, '../')) return console.log('delete done!')
+
+        readdir(src, (err, files) => {
+            if (err) return
+
+            if (!files.length) {
+                rmdir(src, (err) => {
+                    if (err) return
+
+                    recursive(path.resolve(src, '../'))
+                })
+            }
+
+            files.forEach((file) => {
+                const currentPath = path.resolve(src, file)
+
+                stat(currentPath, (err, stats) => {
+                    if (err) return
+
+                    if (stats.isFile()) {
+                        unlink(currentPath, (err) => {
+                            if (err) return
+
+                            recursive(src)
+                        })
+                    } else {
+                        recursive(currentPath)
+                    }
+                })
+            })
+        })
+    })(folder)
+}
+
+function createDir(path, cb) {
+    mkdir(path, (err) => {
+        if (err && err.code !== 'EEXIST') {
+            return cb(err, false)
         }
-    });
-};
 
-const removeDir = (directory, cb) => {
-    const files = fs.readdirSync(directory);
+        cb(null, true)
+    })
+}
 
-    files.forEach(async (file) => {
-        const localBase = path.join(directory, file);
-        const state = fs.statSync(localBase);
+function reader(src) {
+    observer.add(src)
+    readdir(src, (err, files) => {
+        if (err) throw err
 
-        if (state.isDirectory()) {
-            removeDir(localBase);
-        } else {
-            fs.unlinkSync(localBase);
-        }
-    });
+        files.forEach((file) => {
+            const currentPath = path.resolve(src, file)
 
-    fs.rmdir(directory, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-};
+            observer.add(currentPath)
 
-sort(directory, finalPath);
+            stat(currentPath, (err, stats) => {
+                if (err) throw err
 
-if (shouldDelete === "-d") {
-    removeDir(directory);
+                if (stats.isFile()) {
+                    createDir(dist, (err) => {
+                        if (err) throw err
+
+                        const localeDir = path.resolve(dist, file[0].toUpperCase())
+                        const localeFile = path.resolve(localeDir, file)
+
+                        createDir(localeDir, (err) => {
+                            if (err) throw err
+
+                            copyFile(currentPath, localeFile, (err) => {
+                                if (err) throw err
+                                counter++;
+                                console.log('copyFile', `count:${counter}`)
+                                observer.remove(currentPath)
+                            })
+                        })
+                    })
+                } else {
+                    reader(currentPath)
+                    observer.remove(src)
+                }
+            }) 
+        })
+
+        observer.remove(src)
+    })
+}
+
+try {
+    reader(folder)
+} catch (error) {
+    console.log(error)
 }
